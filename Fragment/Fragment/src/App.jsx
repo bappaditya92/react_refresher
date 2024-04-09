c8c98a0c60ee5b2d41031df48cee6d566c260353
{my <{bappadityasinha.92@gmail.com}>
2024-04-09 12:27:58 +0530

    adding some code

---
+++ Fragment/Fragment/src/App.jsx
@@ -0,0 +1,38 @@
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "./App.css";
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <h1>Healthy Food</h1>;
+//       <ul className="list-group">
+//         <li className="list-group-item">Vegetables</li>
+//         <li className="list-group-item">Egg</li>
+//         <li className="list-group-item">Chicken</li>
+//         <li className="list-group-item">Milk</li>
+//         <li className="list-group-item">Fruits</li>
+//       </ul>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>Healthy Food</h1>;
+      <ul className="list-group">
+        <li className="list-group-item">Vegetables</li>
+        <li className="list-group-item">Egg</li>
+        <li className="list-group-item">Chicken</li>
+        <li className="list-group-item">Milk</li>
+        <li className="list-group-item">Fruits</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
