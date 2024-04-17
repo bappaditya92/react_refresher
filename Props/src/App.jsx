65cbc32756fbdfc430990e4136042f4c9dab69b1
{my <{bappadityasinha.92@gmail.com}>
2024-04-17 13:28:42 +0530

    adding some code

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -2,20 +2,13 @@ import "./App.css";
 import Card from "./Component/Card";
 
 function App() {
-  let myObj = {
-    name: "Bappaditya",
-    age: 30,
-    city: "Contai",
-  };
-
-  let myArr = [2, 88, 6, "adi"];
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-5">
         Tailwind Test
       </h1>
-      <Card channel="Chaiaurcode" someObj={myObj} someArr={myArr} />
-      <Card />
+      <Card userName="Chaiaurcode" btnText="Click me" />
+      <Card userName="Mani" />
     </>
   );
 }
--- Props/src/Component/Card.jsx
+++ Props/src/Component/Card.jsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-function Card(props) {
-  console.log("props", props);
+// eslint-disable-next-line react/prop-types
+function Card({ userName, btnText = "Visit me" }) {
+  console.log(userName);
   return (
     <div className="relative h-[400px] w-[300px] rounded-md">
       <img
@@ -12,13 +13,13 @@ function Card(props) {
       />
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <div className="absolute bottom-4 left-4 text-left">
-        <h1 className="text-lg font-semibold text-white">Delba</h1>
+        <h1 className="text-lg font-semibold text-white">{userName}</h1>
         <p className="mt-2 text-sm text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
           debitis?
         </p>
         <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-          View Profile →
+          {btnText} →
         </button>
       </div>
     </div>
