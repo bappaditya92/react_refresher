5e5925d20d09eb2f58130b56c1e95c8c5c8ed8fe
bappaditya92 <bappadityasinha.92@example.com>
2025-04-23 15:00:07 +0530

    add some code

---
+++ react_practice/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+  <h1>This is the first react practice code</h1>
+  <h2>Hello This is the second line </h2>
+
+  </>
+  )
+}
+
+export default App
